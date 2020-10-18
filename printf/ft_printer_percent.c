9dbdea85b79c8d31e8115866ed1b900ddc176c65
agonzale <agonzale@student.42madrid.com>
2020-10-18 12:14:17 +0200

    percent

--- printf/ft_printer_percent.c
+++ printf/ft_printer_percent.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 11:59:32 by user42            #+#    #+#             */
-/*   Updated: 2020/10/18 12:09:48 by user42           ###   ########.fr       */
+/*   Updated: 2020/10/18 12:14:10 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,19 @@
 void    ft_printer_percent(t_list *l, const char *line)
 {
     int number = 0;
-    if (l->width > 0)
+    if (l->flags.minus == TRUE)
     {
-        if (l->flags.minus == TRUE)
-        {
-            ft_putchar('%', l);
-            ft_printer_spaces(l, l->width - 1, line);
-        }
-        else if (l->flags.minus == FALSE && l->flags.zero == FALSE)
-        {
-            ft_printer_spaces(l, l->width - 1, line);
-            ft_putchar('%', l);
-        }
-        else if (l->flags.minus == FALSE && l->flags.zero == TRUE)
-        {
-            ft_printer_zero(l, number, l->width - 1);
-            ft_putchar('%', l);
-        }
+        ft_putchar('%', l);
+        ft_printer_spaces(l, l->width - 1, line);
+    }
+    else if (l->flags.minus == FALSE && l->flags.zero == FALSE)
+    {
+        ft_printer_spaces(l, l->width - 1, line);
+        ft_putchar('%', l);
+    }
+    else if (l->flags.minus == FALSE && l->flags.zero == TRUE)
+    {
+        ft_printer_zero(l, number, l->width - 1);
+        ft_putchar('%', l);
     }
 }
