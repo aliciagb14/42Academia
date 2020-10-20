2843671d4f98e73573895ce2f9732855bc0fdb87
agonzale <agonzale@student.42madrid.com>
2020-10-20 18:02:22 +0200

    more s

--- printf/ft_printer_string.c
+++ printf/ft_printer_string.c
@@ -6,7 +6,7 @@
 /*   By: agonzale <agonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 13:17:26 by agonzale          #+#    #+#             */
-/*   Updated: 2020/10/20 17:56:55 by agonzale         ###   ########.fr       */
+/*   Updated: 2020/10/20 18:02:14 by agonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,11 +93,11 @@ void ft_case_width_s(t_list *l, const char *line, char *str)
 		{
 			if (l->width > l->len)
 			{
+				ft_printer_spaces(l, l->width - l->len, line);
 				ft_printer_character(l, str);
-				ft_printer_spaces(l, l->width, line);	
 			}
 			else
-				ft_printer_spaces(l, l->width, line);	
+				ft_printer_spaces(l, l->width, line);
 		}
 		else
 		{
