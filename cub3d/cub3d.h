a492ecc9830b40bba3d8bcbc4c09c4e285bacf19
Aliciagb <aliciagonzalezb14@gmail.com>
2021-01-16 14:02:52 +0100

    cub3d changes

--- cub3d/cub3d.h
+++ cub3d/cub3d.h
@@ -5,26 +5,63 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: agonzale <agonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/06 00:18:08 by agonzale          #+#    #+#             */
-/*   Updated: 2020/11/09 11:13:52 by agonzale         ###   ########.fr       */
+/*   Created: 2021/01/04 13:51:26 by agonzale          #+#    #+#             */
+/*   Updated: 2021/01/16 13:11:36 by agonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
+# define PI 3.141592
 # include <fcntl.h>
 # include "libft/libft.h"
-# define PI 3.141592
+# include <fcntl.h>
+# include <stdio.h>
+//# include <mlx.h>
+# include <math.h>
+# include <time.h>
 
-typedef struct t_player
+typedef enum s_error_number
 {
-	double	posX;
-	double	posY;
-	double	dirX;
-	double	dirY;
-	double	camX;
-	double	camY;
-	double	time;
-	double	old_time;
-}			s_player;
+	cant_open,
+	wrong_file,
+	wrong_input,
+	repeated_resolution_def,
+	wrong_resolution_x,
+	wrong_resolution_y
+} 			t_error_number;
+
+typedef struct s_error
+{
+	t_error_number error_id;
+	int		row;
+	int		column;
+}			t_error;
+
+typedef struct s_texture
+{
+	
+}			t_texture;
+
+typedef struct s_data
+{
+	int		resolution_x;
+	int		resolution_y;
+	int		floor;
+	int		ceiling;
+	t_texture north;
+	t_texture south;
+	t_texture west;
+	t_texture east;
+}			t_data;
+/*
+typedef struct s_gnl_buffer
+{
+	
+}	t_gnl_buffer;*/
+
+void get_info_map(char *line, t_error error, t_data *info);
+void read_file(char *path, t_data *info);
+int get_resolution(char *line, t_error error, t_data *info);
+void handle_args(int argc, char **argv);
 #endif
--- cub3d/read_map.c
+++ cub3d/read_map.c
@@ -5,41 +5,89 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: agonzale <agonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/05 23:24:57 by agonzale          #+#    #+#             */
-/*   Updated: 2020/11/09 11:12:37 by agonzale         ###   ########.fr       */
+/*   Created: 2021/01/04 13:50:55 by agonzale          #+#    #+#             */
+/*   Updated: 2021/01/16 13:16:13 by agonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define mapWidth 24
-#define mapHeight 24
-#define screenWidth 640
-#define screenHeight 480
 #include "cub3d.h"
 
-int worldMap[mapWidth][mapHeight]=
+void handle_args(int argc, char **argv)
 {
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-};
+    if (argc < 3)
+      ft_putstr_fd("No input files.\n", 1);
+    if (argc == 3 && ft_strncmp(argv[2], "--save", 7))
+      ft_putstr_fd("Error in the second argument.\n", 1);
+    if (argc > 3)
+      ft_putstr_fd("Too much arguments.\n", 1);
+    if (ft_strncmp(argv[1], ".cub", 5))
+      ft_putstr_fd("Error in the name of the file.\n", 1);
+    exit(-1);
+}
+
+int get_resolution(char *line, t_error error, t_data *info)
+{
+    if (info->resolution_x == 0 && info->resolution_y == 0)
+    {
+      if (!(info->resolution_x = ft_atoi(line))) //si no es string de numeros
+        return error.error_id = wrong_resolution_x;
+      else
+      {
+        info->resolution_x = ft_atoi(line);
+        while(ft_isspace(*line + 1))
+        {
+          if (!(info->resolution_y = ft_atoi(line + 1)))
+            return error.error_id = wrong_resolution_y;
+          else
+            info->resolution_y = ft_atoi(line + 1);
+        }
+      }
+    }
+    else
+    {
+      ft_putstr_fd("Resolution alredy added.\n", 1);
+      return error.error_id = repeated_resolution_def;
+    }
+    return (0);
+}
+
+void read_file(char *path, t_data *info)
+{
+    int fd;
+    t_error error;
+    ft_memset(info, 0, sizeof(t_error_number));
+    
+    if (fd = open(path, O_RDONLY) == -1)
+      error.error_id = cant_open;
+}
+
+void get_info_map(char *line, t_error error, t_data *info) //t_gnl_buffer *buffer
+{
+  if (!ft_strncmp(line, "R ", 2))
+    handle_resolution(line + 2, error, &info);
+  else if (!ft_strncmp(line, "C ", 2))
+    handle_colour(line + 2, error, &info->ceiling);
+  else if (!ft_strncmp(line, "F ", 2))
+    handle_colour(line + 2, error, &info->floor);
+  else if (!ft_strncmp(line, "NO ", 3))
+    handle_texture(line + 3, error, &info->north);
+  else if (!ft_strncmp(line, "SO ", 3))
+    handle_texture(line + 3, error, &info->south);
+  else if (!ft_strncmp(line, "WE ", 3))
+    handle_texture(line + 3, error, &info->west);
+  else if (!ft_strncmp(line, "EA ", 3))
+    handle_texture(line + 3, error, &info->east);
+  /*else if (ft_isdigit(*line) || *line == ' ')
+    handle_map(buffer, error, info);*/
+  else
+    error.error_id = wrong_input;
+}
+
+int main(int argc, char **argv)
+{
+    t_data *info;
+    handle_args(argc, argv);
+    ft_memset(&info, 0, sizeof(t_data));
+    read_file(argv[1], &info);
+}
+
